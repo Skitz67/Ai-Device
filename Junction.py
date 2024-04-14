e04dd2a57edbad7cf32f01279ed103739ea287bc
Skitz67 <94258810+Skitz67@users.noreply.github.com>
2024-04-14 17:01:07 +1000

    Fixing and breaking schedular functions
    fixed next task function, some kind of error somewhere within saving to file, not clearing or overwriting file or something. changed save variable to savefile, removed addcallback function from junction, was unnecessary

--- Junction.py
+++ Junction.py
@@ -2,7 +2,6 @@ import time
 import STM
 import scheduler as sch
 
-command = input("Input command: ") #Replace with ai output command
 schedular = sch.schedular()
 #set class 
 class app:
@@ -12,18 +11,12 @@ class app:
 
     #run function from app
     def run(self, args=[]):
-        print(args)
-        if args == [] or args == ['']:
-            
-            return self.callBack()
-            
-        else:
-            
-            return self.callBack(*args)
 
-    def assignCallback(self, callBack):
-        self.callBack = callBack
-        return self
+        if args == [] or args == ['']:
+            return self.callBack()
+        
+        else:
+            return self.callBack(*args)
 
     #instanciate application
     def __init__ (self, signs, Callback):
@@ -36,15 +29,15 @@ class app:
 #list of applications (basically imports)
 apps = []
 #full list of functions from apps
-apps.append(app(["STMQ"], STM.Query).assignCallback(STM.Query))
+apps.append(app(["STMQ"], STM.Query))
 apps.append(app(["STMS"], STM.Search))
 apps.append(app(["STMD"], STM.Delete))
-apps.append(app(["schAdd"], schedular.addToQueue).assignCallback(schedular.addToQueue))
+apps.append(app(["schAdd"], schedular.addToQueue))
 apps.append(app(["schClear"], schedular.clearQueue))
-apps.append(app(["schGetSize"], schedular.queueSize).assignCallback(schedular.queueSize))
+apps.append(app(["schGetSize"], schedular.queueSize))
 apps.append(app(["schNextTask"], schedular.nextTask))
 apps.append(app(["schAllTasks"], schedular.getAllTasks))
-apps.append(app(["schFinishTask"], schedular.finishTask))
+apps.append(app(["schFinishTask"], schedular.finishFirstTask))
 apps.append(app(["schLoad"], schedular.Load))
 apps.append(app(["schReadQueue"], schedular.readQueue))
 
--- scheduler.py
+++ scheduler.py
@@ -9,21 +9,21 @@ class schedular:
 
     #establish queue
     q = queue.PriorityQueue()
-    save = "data.txt"
-    ###@dataclass(order=True)
+    savefile = "data.txt"
+    
 
 
 
     def addToQueue(self, task, Priority = False, shouldManageFiles = True):
         if(shouldManageFiles):
-            self.Load(self.save)
+            self.Load(self.savefile)
         #add task to queue
         if Priority == True:
             try:
                 #add task to front of queue
                 self.q.put_nowait(task)
                 if(shouldManageFiles):
-                    self.Save(self.save)
+                    self.Save(self.savefile)
                 return "Task Added"
             except Exception as e:
                 return "Error Occured", e
@@ -32,7 +32,7 @@ class schedular:
                 #add task to end of queue
                 self.q.put(task)
                 if(shouldManageFiles):
-                    self.Save(self.save)
+                    self.Save(self.savefile)
                 return "Task Added"
             except Exception as e:
                 return "Error Occured", e
@@ -47,7 +47,7 @@ class schedular:
             #clear whole queue
             self.q.queue.clear()
             if(shouldManageFiles):
-                self.Save(self.save)
+                self.Save(self.savefile)
             return "Cleared Queue"
         except Exception as e:
             return "Failed To Clear", e
@@ -57,7 +57,7 @@ class schedular:
 
     def queueSize(self, shouldManageFiles = True):
         if(shouldManageFiles):
-            self.Load(self.save)
+            self.Load(self.savefile)
         try:
             #return number of tasks in queue
             return self.q.qsize()
@@ -66,18 +66,18 @@ class schedular:
         
 
 
-    def nextTask(self, shouldManageFiles = True):
+    def nextTask(self, shouldManageFiles = True, block = True):
         if(shouldManageFiles):
-            self.Load(self.save)
-        ###Broken, removes front item
-        return self.q.queue[0]
+            self.Load(self.savefile)
+        
+        return self.q.get(block)
 
 
 
 
     def getAllTasks(self, shouldManageFiles = True):
         if(shouldManageFiles):
-            self.Load(self.save)
+            self.Load(self.savefile)
         #establish list
         tasks = []
         #add each task to end of the list
@@ -92,15 +92,15 @@ class schedular:
             tasks.append(self.q.queue[i])
         return tasks
 
-    def finishTask(self, task, shouldManageFiles = True):
+    def finishFirstTask(self, shouldManageFiles = True):
         if(shouldManageFiles):
-            self.Load(self.save)
+            self.Load(self.savefile)
         try:
             #remove task from queue
-            self.q.get(task)
-            self.q.task_done(task)
+            #self.q.get()
+            print(self.q.task_done())
             if(shouldManageFiles):
-                self.Save(self.save)
+                self.Save(self.savefile)
             return "Task Finished"
         except Exception as e:
             return "Failed To Finish Task", e
