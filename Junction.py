99f46158c6ca517d743c7b7eeb80b6f86bb61a56
Oscar Jones <S20508@ormiston.qld.edu.au>
2024-04-17 13:21:26 +1000

    Removed redundant error catching and included new error handles
    All errors are not sent through the app interface with the app and arguments given through

--- Junction.py
+++ Junction.py
@@ -3,32 +3,37 @@ import STM
 import scheduler as sch
 
 schedular = sch.schedular()
-#set class 
+
+
+# set class
 class app:
-    #apps, function within apps
+    # apps, function within apps
     callSigns = []
     callBack = None
 
-    #run function from app
+    # run function from app
     def run(self, args=[]):
+        try:
+            if args == [] or args == [""]:
+                return self.callBack()
 
-        if args == [] or args == ['']:
-            return self.callBack()
-        
-        else:
-            return self.callBack(*args)
+            else:
+                return self.callBack(*args)
+        except Exception as e:
+            print(
+                f"An error occured when executing the app '{self.callSigns[0]}' with the arguments '{args}'\nError: {e}"
+            )
 
-    #instanciate application
-    def __init__ (self, signs, Callback):
-        #set app names and function names for instance
+    # instanciate application
+    def __init__(self, signs, Callback):
+        # set app names and function names for instance
         self.callSigns = signs
         self.callBack = Callback
 
 
-
-#list of applications (basically imports)
+# list of applications (basically imports)
 apps = []
-#full list of functions from apps
+# full list of functions from apps
 apps.append(app(["STMQ"], STM.Query))
 apps.append(app(["STMS"], STM.Search))
 apps.append(app(["STMD"], STM.Delete))
@@ -41,16 +46,17 @@ apps.append(app(["schFinishTask"], schedular.finishFirstTask))
 apps.append(app(["schLoad"], schedular.Load))
 apps.append(app(["schReadQueue"], schedular.readQueue))
 
-def imports(appname, arguements = []):
+
+def imports(appname, arguements=[]):
 
     for i in range(len(apps)):
-    #get the current app for itteration
+        # get the current app for itteration
         currentApp = apps[i]
-        #check if command is within the call signs of current app
-        if appname in currentApp.callSigns:    
+        # check if command is within the call signs of current app
+        if appname in currentApp.callSigns:
             return currentApp.run(arguements)
-            
-        
+
+
 print(input("list of args: ").split(" "))
 while True:
     print(imports(input("AppName: "), input("list of args: ").split(" ")))
